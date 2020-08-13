a65d1c8d70398ced05ac7f613c16dd9a443fe0ea
FerF527 <fernandofarias527@gmail.com>
2020-08-12 23:03:47 -0300

    agrego ruteo del carrito de compras

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require("./routes/products");
-
+var carritoDeCompraRouter = require('./routes/carritoDeCompra')
 var loginRouter = require('./routes/login');
 var registroRouter = require('./routes/formularioregistro');
 var app = express();
@@ -27,6 +27,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/products', productsRouter);
 app.use('/registroproductos', registroRouter);
+app.use('/carrito', carritoDeCompraRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
