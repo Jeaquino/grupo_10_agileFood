977d33b5909537856c00907e9b3b2d55131f1681
Jeaquino <julian.eduardo.aquinogmail.com>
2020-08-13 02:24:59 -0300

    index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,10 +2,29 @@ const dbProduct = require("../data/database");
 
 module.exports = {
 
-    detail:function(req, res, next) {
+home:function(req, res, next) {
 
-        res.render('index', { 
-            css:"index"  
-        });
+    let categorias;
+    let mostrar;
+    let catalogo;
+
+    dbProduct.forEach(producto => {
+        if (!categorias.includes(producto.category)){
+            categorias.push(producto.category)
+        }
+    })
+
+    categorias.forEach(categoria => {
+        catalogo.push(dbProducts.filter(producto => {
+            return producto.category == categoria
+        }))
+    })
+
+    res.render('index', { 
+        title: "Agile Food",
+        css:"index",
+        categorias: categorias,
+        catalogo: catalogo,  
+    });
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/indexController")
 
-router.get("/", controller.detail)  // utilizo el metodo listar de productsController
+router.get("/", controller.home)  // utilizo el metodo listar de productsController
 
 module.exports = router;
