41f7e3cb556e6a46ed1ee2e6624be2a95e9c2eae
Jeaquino <julian.eduardo.aquinogmail.com>
2020-10-12 18:44:08 -0300

    modificacion del crud, se actualiza el metodo de crear usuario

--- sitio/data/agilefood.sql
+++ sitio/data/agilefood.sql
@@ -5,9 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema agilefood
 -- -----------------------------------------------------
 
@@ -21,72 +18,58 @@ USE `agilefood` ;
 -- Table `agilefood`.`productos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `agilefood`.`productos` (
-  `idProducto` INT NOT NULL AUTO_INCREMENT,
+  `idProducto` INT(11) NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(100) NOT NULL,
   `precio` DECIMAL(10,0) NOT NULL,
-  `descuento` INT NULL DEFAULT NULL,
+  `descuento` INT(11) NULL DEFAULT NULL,
   `categoria` VARCHAR(100) NULL DEFAULT NULL,
   `clasificacion` VARCHAR(100) NULL DEFAULT NULL,
   `puntaje` DECIMAL(2,2) NULL DEFAULT NULL,
-  `stock` INT NOT NULL,
-  `descripcion` VARCHAR(300) NULL DEFAULT NULL,
-  `imagen` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`idProducto`),
-  CONSTRAINT `producto-carrito`
-    FOREIGN KEY (`idProducto`)
-    REFERENCES `agilefood`.`carritos` (`idProducto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `stock` INT(11) NOT NULL,
+  `descripcion` VARCHAR(300) NOT NULL,
+  `imagen` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idProducto`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `agilefood`.`carritos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agilefood`.`carritos` (
-  `idcarrito` INT NOT NULL AUTO_INCREMENT,
-  `idUsuario` INT NOT NULL,
-  `idProducto` INT NOT NULL,
-  PRIMARY KEY (`idcarrito`),
-  INDEX `carrito-usuario_idx` (`idUsuario` ASC),
-  INDEX `carrito-producto_idx` (`idProducto` ASC),
-  CONSTRAINT `carrito-usuario`
-    FOREIGN KEY (`idUsuario`)
-    REFERENCES `agilefood`.`usuarios` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `carrito-producto`
-    FOREIGN KEY (`idProducto`)
-    REFERENCES `agilefood`.`productos` (`idProducto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `agilefood`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `agilefood`.`usuarios` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT,
+  `idUsuario` INT(11) NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(100) NOT NULL,
   `apellido` VARCHAR(100) NOT NULL,
-  `dni` INT(8) NULL,
+  `dni` INT(8) NULL DEFAULT NULL,
   `email` VARCHAR(100) NOT NULL,
   `constraseña` VARCHAR(100) NOT NULL,
-  `idDomicilio` INT NULL DEFAULT NULL,
+  `idDomicilio` INT(11) NULL DEFAULT NULL,
   `categoria` VARCHAR(100) NULL DEFAULT NULL,
-  `imagen` VARCHAR(100) NULL DEFAULT NULL,
-  INDEX `usuario-carrito_idx` (`idUsuario` ASC),
+  `imagen` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idUsuario`),
-  CONSTRAINT `usuario-carrito`
-    FOREIGN KEY (`idUsuario`)
-    REFERENCES `agilefood`.`carritos` (`idUsuario`)
+  INDEX `usuario-carrito_idx` (`idUsuario` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `agilefood`.`carritos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `agilefood`.`carritos` (
+  `idcarrito` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` INT(11) NOT NULL,
+  `idProducto` INT(11) NOT NULL,
+  PRIMARY KEY (`idcarrito`),
+  INDEX `carrito-usuario_idx` (`idUsuario` ASC),
+  INDEX `carrito-producto_idx` (`idProducto` ASC),
+  CONSTRAINT `carrito-producto`
+    FOREIGN KEY (`idProducto`)
+    REFERENCES `agilefood`.`productos` (`idProducto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `usuario-domicilio`
+  CONSTRAINT `carrito-usuario`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `agilefood`.`domicilios` (`idUsuario`)
+    REFERENCES `agilefood`.`usuarios` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -97,14 +80,14 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `agilefood`.`domicilios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `agilefood`.`domicilios` (
-  `idDomicilio` INT NOT NULL AUTO_INCREMENT,
+  `idDomicilio` INT(11) NOT NULL AUTO_INCREMENT,
   `idUsuario` INT(11) NOT NULL,
   `calle` VARCHAR(100) NOT NULL,
   `altura` INT(11) NOT NULL,
   `departamento` VARCHAR(100) NULL DEFAULT NULL,
   `localidad` VARCHAR(100) NOT NULL,
-  `provincia` VARCHAR(100) NOT NULL,
-  `pais` VARCHAR(100) NOT NULL,
+  `provincia` VARCHAR(100) NULL,
+  `pais` VARCHAR(100) NULL,
   `descripcion` VARCHAR(300) NULL DEFAULT NULL,
   `codigo_postal` INT(11) NOT NULL,
   PRIMARY KEY (`idDomicilio`),
