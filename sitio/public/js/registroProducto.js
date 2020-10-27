7b958569b367d4555f24074f400fd8a4ba22e46b
Jeaquino <julian.eduardo.aquinogmail.com>
2020-10-27 10:49:35 -0300

    se genero la vista previa de registro producto

--- sitio/public/js/registroProducto.js
+++ sitio/public/js/registroProducto.js
@@ -1,16 +1,33 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 
-    let nombre = document.getElementById("input.nombre.value")
-    let precio = document.getElementById("input.nombre.precio")
-    let descuento = document.getElementById("input.nombre.descuento")
-    let titulo = document.querySelector("h2 titulo")
+    let nombre = document.querySelector(".nombre")
+    let titulo = document.querySelector(".titulo")
 
-    document.addEventListener(input, function(){
-        titulo.innerHTML = nombre;
-        titulo.innerHTML = nombre;
-        titulo.innerHTML = nombre;
+    let precio = document.querySelector(".precio")
+    let importe = document.querySelector(".importe")
 
+    let descuento = document.querySelector(".descuento")
+    let rebaja = document.querySelector(".rebaja")
 
+    let imagen = document.querySelector("custom-file-input")
+    console.log(imagen)
+
+    nombre.addEventListener("input", function () {
+        titulo.innerHTML = nombre.value
     })
-    
-    });
+
+    precio.addEventListener("input", function(){
+        importe.innerHTML = precio.value
+    })
+
+    descuento.addEventListener("input", function(){
+        rebaja.innerHTML = precio.value
+    })
+
+    imagen.addEventListener("change", function(){
+        console.log(imagen)
+    })
+
+
+
+});
