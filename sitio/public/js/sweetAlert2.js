fb1d952f565d5f826b68ed011a00780a38d80079
ioryx <ioryxlineage@gmail.com>
2020-10-31 17:22:45 -0300

    sweetalert2 programacion

--- sitio/public/js/sweetAlert2.js
+++ sitio/public/js/sweetAlert2.js
@@ -1,3 +1,60 @@
-Swal.fire({
-    title: "bienvenido!"
+Swal.fire({ /*se trabaja con un objeto por eso se tiene que poner coma despues de los elementos o propiedades*/
+    title: 'bienvenido a Agile food!', //titulo
+    text: 'Espero que te guste la comida deliciosa', //texto debajo del titulo
+    // html:
+    icon: 'success', //icono de success tambien esta de question,information
+    confirmButtonText: 'ok', //mensaje de confirmacion
+    footer: 'Cumpliendo los estandares de Calidad ISO 2001®', //mensaje de pie de pagina
+    // width:
+    // padding:
+    background: '#000',
+    grow: 'fullscreen'
+        // backdrop:
+        // timer:
+        // timerProgressBar:
+        // toast:
+        // position:
+        // allowOutsideClick:
+        // allowEscapeKey:
+        // allowEnterKey:
+        // stopKeydownPropagation:
+
+    // input:
+    // inputPlaceholder:
+    // inputValue:
+    // inputOptions:
+
+    //  customClass:
+    // 	container:
+    // 	popup:
+    // 	header:
+    // 	title:
+    // 	closeButton:
+    // 	icon:
+    // 	image:
+    // 	content:
+    // 	input:
+    // 	actions:
+    // 	confirmButton:
+    // 	cancelButton:
+    // 	footer:	
+
+    // showConfirmButton:
+    // confirmButtonColor:
+    // confirmButtonAriaLabel:
+
+    // showCancelButton:
+    // cancelButtonText:
+    // cancelButtonColor:
+    // cancelButtonAriaLabel:
+
+    // buttonsStyling:
+    // showCloseButton:
+    // closeButtonAriaLabel:
+
+
+    // imageUrl:
+    // imageWidth:
+    // imageHeight:
+    // imageAlt:
 });
