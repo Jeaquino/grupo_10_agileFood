28e2de2792b4e17fd9dc947eb6d97637089e0be4
ioryx <ioryxlineage@gmail.com>
2020-11-04 11:01:02 -0300

    previa de pass

--- sitio/public/js/login.js
+++ sitio/public/js/login.js
@@ -1,25 +1,25 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
 
     let mail = document.querySelector(".mail")
     let constrasena = document.querySelector(".titulo")
 
-    let regExMail =  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    let regExMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
     let regExContrsena = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,18}$/; // La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos.
 
-    constrasena.addEventListener("blur", function () {
-        if (!regExContrsena.test(this.value)){
-        errorEmail.innerHTML = "La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos"
+    constrasena.addEventListener("blur", function() {
+        if (!regExContrsena.test(this.value)) {
+            errorEmail.innerHTML = "La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos"
         }
     })
 
-    nombre.addEventListener("blur", function () {
+    nombre.addEventListener("blur", function() {
         switch (true) {
             case this.length == 0:
                 errors.nombre = "El campo nombre es obligatorio"
                 this.classList.add('is-invalid')
                 break;
-            case this.value.trim().length <=2:
+            case this.value.trim().length <= 2:
                 errorNombre.innerHTML = "Tenés que poner al menos tres letras"
                 this.classList.add('is-invalid')
                 break
@@ -31,40 +31,59 @@ window.addEventListener('load', function () {
         }
     })
 
-    precio.addEventListener("input", function(){
+    precio.addEventListener("input", function() {
         importe.innerText = precio.value
         console.log(importe)
     })
 
-    descuento.addEventListener("input", function(){
+    descuento.addEventListener("input", function() {
         rebaja.innerText = precio.value
     })
 
-    descripcion.addEventListener("input", function(){
+    descripcion.addEventListener("input", function() {
         detalle.innerText = descripcion.value
     })
 
-    imagen.addEventListener('change',function(e){
+    imagen.addEventListener('change', function(e) {
 
         let reader = new FileReader();
 
         reader.readAsDataURL(e.target.files[0]);
 
-        reader.onload = function(){
+        reader.onload = function() {
             console.log(vistaPrevia.src)
             console.log(e.target.files[0])
             vistaPrevia.src = reader.result;
-        }      
+        }
     })
 
-    botonCategoria.addEventListener("click", function(){
+    botonCategoria.addEventListener("click", function() {
         let categoria = prompt("ingrese el nombre de la categoria")
         let resultado = confirm("seguro que desea agregar la categoria")
     })
 
-    botonClasificacion.addEventListener("click", function(){
+    botonClasificacion.addEventListener("click", function() {
         let clasificacion = prompt("ingrese el nombre de la categoria")
         let resultado = confirm("seguro que desea agregar la categoria")
     })
 
 });
+window.addEventListener('load', function() {
+    const eye = document.querySelector(".feather-eye");
+    const eyeoff = document.querySelector(".feather-eye-off");
+    const passwordField = document.querySelector("input[type=password]");
+
+    eye.addEventListener("click", () => {
+        eyeoff.style.display = "block";
+        eye.style.display = "none";
+
+        passwordField.type = "text";
+    });
+
+    eyeoff.addEventListener("click", () => {
+        eyeoff.style.display = "none";
+        eye.style.display = "block";
+
+        passwordField.type = "password";
+    })
+})
