4fc221409fb138480e0482e365adfb2ffdb51584
ioryx <ioryxlineage@gmail.com>
2020-11-04 04:22:53 -0300

    comentarios en carpeta database/config.js y models/index.js

--- sitio/database/config/config.js
+++ sitio/database/config/config.js
@@ -1,26 +1,29 @@
-module.exports =
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "agilefood",
-    "host": "127.0.0.1",
-    "port": "3306",
-    "dialect": "mysql",
-    "operatorAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+module.exports = //primero se agrega module.exports es fundamental para no tener errores 
+    //luego tenemos que configurar los datos de nuesra base de datos
+    //en este caso configuraremos de desarrollo(development)
+    {
+        "development": {
+            "username": "root", //usuario
+            "password": null, //contraseña
+            "database": "agilefood", //la base de datos se llama
+            "host": "127.0.0.1", //donde queda la base de datos
+            "port": "3306", //donde queda el puerto
+            "dialect": "mysql", //que idioma maneja la base de datos
+            "operatorAliases": false
+        }, //sequelizer toma todo estos datos para hacer la coneccion con javascript eso lo hace en 
+        // la carpeta (models) en el archivo(index.js)
+        "test": {
+            "username": "root",
+            "password": null,
+            "database": "database_test",
+            "host": "127.0.0.1",
+            "dialect": "mysql"
+        },
+        "production": {
+            "username": "root",
+            "password": null,
+            "database": "database_production",
+            "host": "127.0.0.1",
+            "dialect": "mysql"
+        }
+    }
