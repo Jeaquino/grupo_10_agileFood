ed8f96c59bf274abdeecb08c8923c43ade0bf5f5
Jeaquino <julian.eduardo.aquino@gmail.com>
2020-11-05 09:39:41 -0300

    validaciones front y back de login

--- sitio/public/js/login.js
+++ sitio/public/js/login.js
@@ -1,26 +1,20 @@
 window.addEventListener('load', function() {
 
     let mail = document.querySelector(".mail")
-    let constrasena = document.querySelector(".titulo")
+    let contrasena = document.querySelector(".contrasena")
 
     let regExMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-    let regExContrsena = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,18}$/; // La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos.
+    let regExContrasena = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,18}$/; // La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos.
 
-    constrasena.addEventListener("blur", function() {
-        if (!regExContrsena.test(this.value)) {
-            errorEmail.innerHTML = "La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos"
-        }
-    })
-
-    nombre.addEventListener("blur", function() {
+    mail.addEventListener("blur", function() {
         switch (true) {
             case this.length == 0:
-                errors.nombre = "El campo nombre es obligatorio"
+                errorEmail = "El campo no puede estar vacio, ingrese un mail"
                 this.classList.add('is-invalid')
                 break;
-            case this.value.trim().length <= 2:
-                errorNombre.innerHTML = "Tenés que poner al menos tres letras"
+            case (!regExMail.test(this.value)):
+                errorEmail.innerHTML = "Debe ingresar un mail valido"
                 this.classList.add('is-invalid')
                 break
             default:
@@ -31,59 +25,21 @@ window.addEventListener('load', function() {
         }
     })
 
-    precio.addEventListener("input", function() {
-        importe.innerText = precio.value
-        console.log(importe)
-    })
-
-    descuento.addEventListener("input", function() {
-        rebaja.innerText = precio.value
-    })
-
-    descripcion.addEventListener("input", function() {
-        detalle.innerText = descripcion.value
-    })
-
-    imagen.addEventListener('change', function(e) {
-
-        let reader = new FileReader();
-
-        reader.readAsDataURL(e.target.files[0]);
-
-        reader.onload = function() {
-            console.log(vistaPrevia.src)
-            console.log(e.target.files[0])
-            vistaPrevia.src = reader.result;
+    contrasena.addEventListener("blur", function() {
+        switch (true) {
+            case this.length == 0:
+                errorContrasena = "El campo no puede estar vacio, ingrese una contraseña"
+                this.classList.add('is-invalid')
+                break;
+            case (!regExContrasena.test(this.value)):
+                errorContrasena.innerHTML = "La contraseña debe tener al entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos"
+                this.classList.add('is-invalid')
+                break
+            default:
+                this.classList.remove('is-invalid')
+                this.classList.add('is-valid')
+                errorNombre.innerHTML = ""
+                break;
         }
-    })
-
-    botonCategoria.addEventListener("click", function() {
-        let categoria = prompt("ingrese el nombre de la categoria")
-        let resultado = confirm("seguro que desea agregar la categoria")
-    })
-
-    botonClasificacion.addEventListener("click", function() {
-        let clasificacion = prompt("ingrese el nombre de la categoria")
-        let resultado = confirm("seguro que desea agregar la categoria")
-    })
-
-});
-window.addEventListener('load', function() {
-    const eye = document.querySelector(".feather-eye");
-    const eyeoff = document.querySelector(".feather-eye-off");
-    const passwordField = document.querySelector("input[type=password]");
-
-    eye.addEventListener("click", () => {
-        eyeoff.style.display = "block";
-        eye.style.display = "none";
-
-        passwordField.type = "text";
-    });
-
-    eyeoff.addEventListener("click", () => {
-        eyeoff.style.display = "none";
-        eye.style.display = "block";
-
-        passwordField.type = "password";
     })
 })
--- sitio/validation/loginValidator.js
+++ sitio/validation/loginValidator.js
@@ -2,52 +2,59 @@ const {check,validatorResult,body} = require('express-validator');
 const bcrypt = require('bcrypt');
 const db = require('../database/models');
 
+let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
+
 module.exports = [
+    check('email')
+    .isLength({
+        min:1
+    })
+    .withMessage('En campo no debe estar vacio'),
+
     check('email')
     .isEmail()
     .withMessage('Ingrese un email válido'),
 
-    check('contraseña')
-    .isLength(8,18)
-    .isAlphanumeric()
-    .withMessage('Debes ingresar una contraseña con un minimo de 8 caracteres y un maximo de 18'),
-
-    check('contraseña')
-    .isAlphanumeric()
-    .withMessage('Debe poseer valores alphanumericos'),
-
     body('email')
-    .custom(function(value){
+    .custom(function (value) {
         return db.usuarios.findOne({
-            where:{
-                email:value
-            }
-        })
-        .then(user => {
-            if(!user){
-                return Promise.reject('Email no registrado')
-            }
-        })
-        .catch( error => {
-            res.send(error)
-        })
+                where: {
+                    email: value
+                }
+            })
+            .then(user => {
+                if (!user) {
+                    withMessage('el mail no se encuentra registrado')
+                }
+            })
+            .catch(() => {
+                return Promise.reject('el mail no se encuentra registrado')
+            })
     }),
 
+    check('contraseña')
+    .isLength({
+        min:1
+    })
+    .withMessage('En campo no debe estar vacio'),
+
     body('contraseña')
-    .custom((value,{req})=>{
+    .custom((value, {
+        req
+    }) => {
         return db.usuarios.findOne({
-            where:{
-                email:req.body.email
-            }
-        })
-        .then(user => {
-            if(!bcrypt.compareSync(value,user.dataValues.contrasena)){ //si no machea la contraseña
-                return Promise.reject('estas mal')
-            }
-        })
-        .catch(() => {
-            return Promise.reject('Credenciales inválidas')
-        })
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then(user => {
+                if (!bcrypt.compareSync(value, user.dataValues.contrasena)) { //si no machea la contraseña
+                    return Promise.reject('estas mal')
+                }
+            })
+            .catch(() => {
+                return Promise.reject('Contraseña incorrecta')
+            })
     })
 
 ]
