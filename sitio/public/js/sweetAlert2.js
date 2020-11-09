ab5f86030c2e8795fa00a5100833bf88753b7442
ioryx <ioryxlineage@gmail.com>
2020-11-09 04:15:34 -0300

    modelado de sweetalert2 como publicidad en el inicio

--- sitio/public/js/sweetAlert2.js
+++ sitio/public/js/sweetAlert2.js
@@ -6,39 +6,39 @@ Swal.fire({ /*se trabaja con un objeto por eso se tiene que poner coma despues d
     confirmButtonText: 'ok', //mensaje de confirmacion
     footer: 'Cumpliendo los estandares de Calidad ISO 2001®', //mensaje de pie de pagina
     // width:
-    // padding:
+    padding: "1rem",
     background: '#000',
-    grow: 'fullscreen'
-        // backdrop:
-        // timer:
-        // timerProgressBar:
-        // toast:
-        // position:
-        // allowOutsideClick:
-        // allowEscapeKey:
-        // allowEnterKey:
-        // stopKeydownPropagation:
+    grow: '50%', //se cambia para que abarque todo el ancho de la pagina si fuera fullscreen
+    //backdrop: true,
+    timer: 15000, //el tiempo de la alerta que se muestra 5000 mililsegundos son 5 segundos, luego desaparece
+    timerProgressBar: true, //el tiempo de progreso de carga de la aplicacion
+    // toast:
+    position: "center", //para que se posicione en el centro de la pantalla, top arriba,bottom-start aparezca donde comienza,bottom-end es abajo a la derecha
+    allowOutsideClick: true, //esto es si quieron permitir que el usuario haga click afuera de la ventana y sea posible cerrar el alerta
+    allowEscapeKey: false, //quiero permitir que el usuario use la tecla de escape para cerrar
+    allowEnterKey: false, //si el usuario pueda dar enter dentro de la alerta para cerrala.
+    stopKeydownPropagation: false, //averiguar esto es un evento de teclado de fondo
 
     // input:
     // inputPlaceholder:
     // inputValue:
     // inputOptions:
 
-    //  customClass:
-    // 	container:
-    // 	popup:
-    // 	header:
-    // 	title:
-    // 	closeButton:
-    // 	icon:
-    // 	image:
-    // 	content:
-    // 	input:
-    // 	actions:
-    // 	confirmButton:
-    // 	cancelButton:
-    // 	footer:	
-
+    /*customClass: {
+        // 	container:
+        // 	popup:
+        // 	header:
+        // 	title:
+        // 	closeButton:
+        // 	icon:
+        //image: ("../images/pizzachorreando.gif"),
+        // 	content:
+        // 	input:
+        // 	actions:
+        // 	confirmButton:
+        // 	cancelButton:
+        // 	footer:	
+    }*/
     // showConfirmButton:
     // confirmButtonColor:
     // confirmButtonAriaLabel:
@@ -53,8 +53,21 @@ Swal.fire({ /*se trabaja con un objeto por eso se tiene que poner coma despues d
     // closeButtonAriaLabel:
 
 
-    // imageUrl:
+    imageUrl: "../images/pizzachorreando.gif",
     // imageWidth:
     // imageHeight:
     // imageAlt:
 });
+/*Swal.fire({
+    title: 'Custom width, padding, background.',
+    width: 600,
+    padding: '3em',
+    background: '#fff url(/images/trees.png)',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/pizzachorreando.gif")
+      left top
+      no-repeat
+    `
+});
+*/
